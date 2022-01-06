eef29c9a26e2c3e6f4dafb4754844bbf5342cd66
yan.barreiro <yan.barreiro@cubos.io>
2022-01-06 14:43:59 +0000

    feat: create user

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,3 +1,4 @@
+import { getRepository, ILike, Repository } from "typeorm";
 import { Specification } from "../../entities/Specification";
 import {
   ICreateSpecificationsDTO,
@@ -5,22 +6,24 @@ import {
 } from "../interfaces/ISpecificationsRepository";
 
 export class SpecificationsRepository implements ISpecificationsRepository {
-  private specifications: Specification[];
+  private repository: Repository<Specification>;
 
   constructor() {
-    this.specifications = [];
+    this.repository = getRepository(Specification);
   }
 
-  create({ description, name }: ICreateSpecificationsDTO): void {
-    const specification = new Specification();
-    Object.assign(specification, { name, description, created_at: new Date() });
+  async create({ description, name }: ICreateSpecificationsDTO): Promise<void> {
+    const specification = this.repository.create({
+      description,
+      name,
+    });
 
-    this.specifications.push(specification);
+    await this.repository.save(specification);
   }
-  findByName(name: string): Specification {
-    const specification = this.specifications.find(
-      (specification) => specification.name === name
-    );
+  async findByName(name: string): Promise<Specification> {
+    const specification = await this.repository.findOne({
+      name: ILike(`${name}`),
+    });
     return specification;
   }
 }
