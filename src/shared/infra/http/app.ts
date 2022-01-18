821609ee30ebca9f002cea778d62dcd7515d8598
Yansb <yansbarreiro@gmail.com>
2022-01-18 13:24:41 +0000

    feat: add sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -9,6 +9,8 @@ import swaggerUi from "swagger-ui-express";
 import swaggerFile from "../../../swagger.json";
 import { AppError } from "@shared/errors/AppError";
 import upload from "@config/upload";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import rateLimiter from "@shared/infra/http/middlewares/rateLimiter";
 
 import createConnection from "@shared/infra/typeorm";
@@ -17,6 +19,18 @@ const app = express();
 createConnection();
 
 app.use(rateLimiter);
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  tracesSampleRate: 1.0,
+});
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use(express.json());
 
@@ -28,6 +42,8 @@ app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 app.use(cors());
 
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
